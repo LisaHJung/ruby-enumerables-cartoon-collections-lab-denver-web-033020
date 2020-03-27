1a21caf49f7afd12d16bbc6bbfd619073f0d3c79
Lisa Jung <lisahjung@gmail.com>
2020-03-27 20:02:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,65 @@
+
+def roll_call_dwarves(array) 
+  i=0
+  array.each_with_index {|dwarf,i| puts "#{i+1}. #{dwarf}"}
+end
+
+
+def summon_captain_planet (planeteer_calls)
+  planeteer_calls.map do |call|
+    "#{call.capitalize}!"
+  end
+end
+
+def long_planeteer_calls(array_calls)
+  array_calls.map do |call| 
+    if call.length> 4
+    return true
+  end
+  end 
+  return false
+end
+
+def find_the_cheese # code an argument here
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
 def roll_call_dwarves(array)
   i = 0
   array.each_with_index {|name, i| puts "#{i + 1}. #{name} "}
@@ -22,17 +84,7 @@ def find_the_cheese(array)
     cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
   end
 end
-
-=begin cheese_types = ["cheddar", "gouda", "camembert"]
-  def find_the_cheese(array)
-  =end
-
-
-
-
-
-
-
+=end
 
 
 
